40723bb838b459f50a4de262b5196a8b8db68e8d
13579xxl <13807918069@qq.com>
2025-11-09 22:06:15 +0800

    var

---
+++ static/var_example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>//头文件，包含了输出函数
+//main函数，C程序执行的入口
+int main() {
+    //定义变量并赋值
+    int var = 1;
+    //输出var变量
+    printf("The number is: %d\n", var);
+    //每个语句最后必须加分号
+    //最后返回0作为函数的返回值
+    return 0;
+}
